5d7858d69cd42cec304e6780c60cb759253e9236
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2022-02-08 09:43:58 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+class SayHello extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello {this.props.name}</Text>
+      </View>
+    )
+  };
+}
+
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <View>
+        <SayHello name="Waleed!!!" />
+      </View>
+    );
+  }
+}
+
+
+export default HelloWorldApp
---
+++ README.md
@@ -0,0 +1,2 @@
+# Spacebook-app
+ 
