9ed9acaa89a1fa93240cd5d32b2dd703ff7b8e29
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2022-03-08 10:55:20 +0000

    Adding the screens to the app

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './components/login';
 import SignUpScreen from './components/signup';
 import OtherScreens from './components/otherscreens';
+import PostScreens from './components/postscreen';
+import ViewPosts from './components/posts';
+import AddNewPostScreen from './components/addnewpost';
+import UpdatePostScreen from './components/updatepost';
+import ViewPostScreen from './components/viewpost';
+import AddNewFriendPostScreen from './components/addtofriendspost';
 
 
 const Stack = createNativeStackNavigator();
@@ -20,6 +26,11 @@ class App extends Component {
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="SignUp" component={SignUpScreen} />
           <Stack.Screen name="Main" component={OtherScreens} />
+          <Stack.Screen name="Post" component={ViewPosts} />
+          <Stack.Screen name="AddNewPost" component={AddNewPostScreen} />
+          <Stack.Screen name="UpdatePost" component={UpdatePostScreen} />
+          <Stack.Screen name="ViewPost" component={ViewPostScreen} />
+          <Stack.Screen name="AddNewFriendPost" component={AddNewFriendPostScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     );
