31282a97b646b4841db93f75d0719dab4afd110e
waleed-ahmed123 <80536873+waleed-ahmed123@users.noreply.github.com>
2022-03-18 10:25:02 +0000

    cleaning up the code and adding comments

--- components/myfriends.js
+++ components/myfriends.js
@@ -20,46 +20,16 @@ class MyFriendsScreen extends Component {
     }
 
     async componentDidMount() {
-        //this.get_friends_list()
+        // gets friend list each time user loads this screen
         this.unsubscribe = this.props.navigation.addListener('focus', async () => {
             await this.get_friends_list();
         });
         this.setState({
-            //photo: this.get_profile_image(this.props.route.params.item.user_id)
             photo: null
         })
-        //console.log('route' + this.props.route.params)
     }
 
-
-    get_profile_image = async (user_id) => {
-        let details = await AsyncStorage.getItem('@spacebook_details')
-        let parsed_details = JSON.parse(details)
-        let id = parsed_details.id
-        let token = parsed_details.token
-
-        console.log("here", id, token)
-        fetch("http://localhost:3333/api/1.0.0/user/" + user_id + "/photo", {
-            method: 'GET',
-            headers: {
-                "X-Authorization": token,
-                "Content-Type": "image/jpeg"
-            }
-        })
-            .then((res) => {
-                return res.blob();
-            })
-            .then((resBlob) => {
-                let data = URL.createObjectURL(resBlob);
-                this.setState({
-                    photo: data
-                });
-            })
-            .catch((err) => {
-                console.log("error, something went wrong", err)
-            });
-    }
-
+    // gets the friends list and stores the results in details array
     get_friends_list = async () => {
         let details = await AsyncStorage.getItem('@spacebook_details')
         let parsed_details = JSON.parse(details)
@@ -106,52 +76,6 @@ class MyFriendsScreen extends Component {
             });
     }
 
-    get_posts = async (id) => {
-        let details = await AsyncStorage.getItem('@spacebook_details')
-        let parsed_details = JSON.parse(details)
-        //let id = parsed_details.id
-        let token = parsed_details.token
-        fetch('http://localhost:3333/api/1.0.0/user/' + id + '/post', {
-            method: 'GET',
-            headers: {
-                'X-Authorization': token,
-                //'Content-Type': 'application/json'
-            }
-        })
-            .then((response) => {
-                if (response.status === 200) {
-                    return response.json()
-                        .then(async (json) => {
-                            console.log("Here Are Your Friends Posts...")
-                            console.log(json);
-                            console.log(this.details)
-                            this.setState({
-                                posts: json,
-                                isLoading: false
-                            });
-                        })
-                }
-                if (response.status === 401) {
-                    console.log("Unauthorised")
-                    //this.error = true;
-                }
-                if (response.status === 403) {
-                    console.log("Can only view posts of yourself or your friends")
-                    //this.error = true;
-                }
-                if (response.status === 404) {
-                    console.log("Not found")
-                    //this.error = true;
-                }
-                if (response.status === 500) {
-                    console.log("server error");
-                }
-            })
-
-            .catch((error) => {
-                console.error(error);
-            });
-    }
     render() {
         if (this.state.isLoading) {
             return (<View><Text>loading </Text></View>)
@@ -169,14 +93,17 @@ class MyFriendsScreen extends Component {
                                 <Text style={styles.titleText}>Last Name: {JSON.stringify(item.user_familyname)}</Text>
                                 <Text style={styles.titleText}>Email: {JSON.stringify(item.user_email)}</Text>
                                 <Button
+                                    // when user clicks on the button, it navigates to the post screen, and passes the details of the user as well as the friend id in the route
                                     title="View Post"
                                     onPress={() => this.props.navigation.navigate('Post', item = { item, user_id: JSON.stringify(item.user_id) })}
                                 />
                                 <Button
+                                    // when user clicks on the button, it navigates to the add new post to friend screen, and passes the details of the user in the route
                                     title="Add New Post"
                                     onPress={() => this.props.navigation.navigate('AddNewFriendPost', item = {item})}
                                 />
                                 <Button
+                                     // when user clciks on the button, it navigates to the users friends screen, and passes the details of the user as well as the friend id in the route
                                     title="Friends"
                                     onPress={() => this.props.navigation.navigate('UsersFriends', item = { item, user_id: JSON.stringify(item.user_id)  })}
                                 />
